ffd752949ce36593f639d75a4f33b368e802fd52
luis <lui.fenixand.1997@gmail.com>
2026-06-17 08:42:50 -0500

    Agregadas algunos componentes y layouts ademas de tipografias y demas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // site: 'https://LuiO03.github.io',
+    // base: '/my_portfolio/',
+});
