7780bf7132df699e75aaf6a983c25a8d7e7f77cb
luis <lui.fenixand.1997@gmail.com>
2026-06-18 10:49:12 -0500

    algunos cambios en astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luio03.github.io',
-  base: '/my_portfolio',
+  base: process.env.NODE_ENV === 'production'
+    ? '/my_portfolio'
+    : '/',
   integrations: [icon()],
 });
