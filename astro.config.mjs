4455acdfe8d4abb33d9ef0e4ceeba3a6f293cfcc
luis <lui.fenixand.1997@gmail.com>
2026-06-18 11:13:05 -0500

    Prueba de la raiz en github

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://luio03.github.io',
-  base: process.env.NODE_ENV === 'production'
-    ? '/my_portfolio'
-    : '/',
+  site: 'https://luio03.github.io/my_portfolio',
   integrations: [icon()],
 });
